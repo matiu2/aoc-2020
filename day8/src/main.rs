7b5809ced9383125ba7f63576543fcb3b444ff82
Matthew Sherborne <msherborne@gmail.com>
2021-01-18 00:57:43 +1000

    Day 8 Part 1 Complete

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::{hash_set, HashSet};
+use std::{
+    collections::HashSet,
+    fs::{read_link, read_to_string},
+};
 
 use parse_display::{Display, FromStr};
 
@@ -7,7 +10,8 @@ use parse_display::{Display, FromStr};
 enum Instruction {
     #[display("acc {0}")]
     Acc(i64),
-    Nop,
+    #[display("nop {0}")]
+    Nop(i64),
     #[display("jmp {0}")]
     Jmp(i64),
 }
@@ -25,36 +29,46 @@ fn parse(input: &str) -> Vec<Instruction> {
 
 fn execute(instructions: &[Instruction]) -> i64 {
     // Keep track of previous instructions
-    let previous_instructions = HashSet::new();
+    let mut previous_instructions = HashSet::new();
     // Keep track of the accumulator
     let mut acc = 0;
     // Keep track of the instruction pointer
-    let mut ip = 0;
+    let mut ip = 0i64;
     // loop until done
-    while Some(instruction) = instructions.get(ip) {
-        if previous_instructions.contains(ip) {
+    while let Some(instruction) = instructions.get(ip as usize) {
+        if previous_instructions.contains(&ip) {
             break;
         }
-        match ip {
-            Instruction::Acc(num) => acc + num,
-            Instruction::Jmp(num) => ip + num,
-            Instruction::Nop(_) => ip + num,
-        }
+        previous_instructions.insert(ip);
+        match instruction {
+            Instruction::Acc(num) => {
+                acc += num;
+                ip += 1;
+            }
+            Instruction::Jmp(num) => {
+                ip += num;
+            }
+            Instruction::Nop(_) => {
+                ip += 1;
+            }
+        };
     }
     acc
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").expect("Unable to read input.txt");
+    let instructions = parse(&input);
+    let answer = execute(&instructions);
+    println!("Day 7 - Part 1 - Accumulator = {}", answer);
 }
 
 #[cfg(test)]
 mod tests {
     use super::Instruction;
 
-    #[test]
-    fn test_parsing() {
-        let input = r#"nop +0
+    fn input() -> &'static str {
+        r#"nop +0
 acc +1
 jmp +4
 acc +3
@@ -62,7 +76,11 @@ jmp -3
 acc -99
 acc +1
 jmp -4
-acc +6"#;
+acc +6"#
+    }
+
+    #[test]
+    fn test_parsing() {
         let expected = vec![
             Instruction::Nop(0),
             Instruction::Acc(1),
@@ -74,7 +92,15 @@ acc +6"#;
             Instruction::Jmp(-4),
             Instruction::Acc(6),
         ];
-        let got = super::parse(input);
+        let got = super::parse(input());
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn test_execute() {
+        let instructions = super::parse(input());
+        let got = super::execute(&instructions);
+        let expected = 5;
         assert_eq!(got, expected);
     }
 }

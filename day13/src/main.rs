6f3b102733444f4f0225d1d7610efa39191f0153
Matthew Sherborne <msherborne@gmail.com>
2021-01-23 09:05:19 +1000

    Day13 Now uses anyhow for errors instead of unwrap

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,14 +1,15 @@
+use anyhow::{anyhow, Result};
 use std::fs::read_to_string;
 
 /// Parses the puzzle input and returns (earliest_you_can_leave, bus_ids)
-fn parse(input: &str) -> (usize, Vec<usize>) {
+fn parse(input: &str) -> Result<(usize, Vec<usize>)> {
     let lines: Vec<&str> = input.lines().collect();
     assert_eq!(lines.len(), 2);
     // The earliest I can leave
-    let earliest: usize = lines[0].parse().unwrap();
+    let earliest: usize = lines[0].parse()?;
     // The IDs of the busses
     let bus_ids: Vec<usize> = lines[1].split(',').flat_map(|id| id.parse().ok()).collect();
-    (earliest, bus_ids)
+    Ok((earliest, bus_ids))
 }
 
 //// Returns (bus_id, minutes you have to wait (after `earliest`))
@@ -20,35 +21,41 @@ fn earliest_bus_id(earliest: usize, bus_ids: &[usize]) -> Option<(usize, usize)>
         .map(|(&bus_id, to_wait)| (bus_id, to_wait))
 }
 
-fn main() {
-    let input = read_to_string("input.txt").unwrap();
-    let (earliest, bus_ids) = parse(&input);
-    let (bus_id, to_wait) = earliest_bus_id(earliest, &bus_ids).unwrap();
+fn main() -> Result<()> {
+    let input = read_to_string("input.txt")?;
+    let (earliest, bus_ids) = parse(&input)?;
+    let (bus_id, to_wait) =
+        earliest_bus_id(earliest, &bus_ids).ok_or_else(|| anyhow!("Probably there 0 bus_ids"))?;
     let answer = to_wait * bus_id;
     println!(
         "Day13 - Part 1 - Bus ID: {} minutes to wait: {} answer: {}",
         bus_id, to_wait, answer
     );
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use anyhow::{anyhow, Result};
 
     #[test]
-    fn test_parse() {
+    fn test_parse() -> Result<()> {
         let input = "939
 7,13,x,x,59,x,31,19";
-        let (earliest, bus_ids) = super::parse(input);
+        let (earliest, bus_ids) = super::parse(input)?;
         assert_eq!(earliest, 939);
         assert_eq!(bus_ids, vec![7, 13, 59, 31, 19]);
+        Ok(())
     }
 
     #[test]
-    fn test_earliest_bus_id() {
+    fn test_earliest_bus_id() -> Result<()> {
         let earliest = 939;
         let bus_ids = vec![7, 13, 59, 31, 19];
-        let (bus_id, to_wait) = super::earliest_bus_id(earliest, &bus_ids).unwrap();
+        let (bus_id, to_wait) = super::earliest_bus_id(earliest, &bus_ids)
+            .ok_or_else(|| anyhow!("Probably there are no bus_ids"))?;
         assert_eq!(bus_id, 59);
         assert_eq!(to_wait, 5);
+        Ok(())
     }
 }

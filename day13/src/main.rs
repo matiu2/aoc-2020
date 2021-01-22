6ce51b0385f4b3d1a887fafd80fbbcae4458db01
Matthew Sherborne <msherborne@gmail.com>
2021-01-23 08:57:51 +1000

    Day13 - Part 1 - Done

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -11,9 +11,24 @@ fn parse(input: &str) -> (usize, Vec<usize>) {
     (earliest, bus_ids)
 }
 
+//// Returns (bus_id, minutes you have to wait (after `earliest`))
+fn earliest_bus_id(earliest: usize, bus_ids: &[usize]) -> Option<(usize, usize)> {
+    bus_ids
+        .iter()
+        .map(|bus_id| (bus_id, bus_id - (earliest % bus_id)))
+        .min_by_key(|(_bus_id, to_wait)| *to_wait)
+        .map(|(&bus_id, to_wait)| (bus_id, to_wait))
+}
+
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let (earliest, bus_ids) = parse(&input);
+    let (bus_id, to_wait) = earliest_bus_id(earliest, &bus_ids).unwrap();
+    let answer = to_wait * bus_id;
+    println!(
+        "Day13 - Part 1 - Bus ID: {} minutes to wait: {} answer: {}",
+        bus_id, to_wait, answer
+    );
 }
 
 #[cfg(test)]
@@ -26,5 +41,14 @@ mod tests {
         let (earliest, bus_ids) = super::parse(input);
         assert_eq!(earliest, 939);
         assert_eq!(bus_ids, vec![7, 13, 59, 31, 19]);
+    }
+
+    #[test]
+    fn test_earliest_bus_id() {
+        let earliest = 939;
+        let bus_ids = vec![7, 13, 59, 31, 19];
+        let (bus_id, to_wait) = super::earliest_bus_id(earliest, &bus_ids).unwrap();
+        assert_eq!(bus_id, 59);
+        assert_eq!(to_wait, 5);
     }
 }

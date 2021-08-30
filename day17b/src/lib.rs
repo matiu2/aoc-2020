35958d7a3f3c4695441841d4882c71fb341bd624
Matthew Sherborne <msherborne@gmail.com>
2021-08-30 15:24:06 +1000

    day17b - const generics. Nice

--- day17b/src/lib.rs
+++ day17b/src/lib.rs
@@ -1,27 +1,37 @@
 /// A point in space
-pub struct Point {
+#[derive(Debug)]
+pub struct Point<const N: usize> {
     /// In order, x,y,z,w, etc..
-    coordinates: Vec<i64>,
+    coordinates: [i64; N],
 }
 
-impl Point {
+impl<const N: usize> Point<N> {
+    pub fn new(coordinates: [i64; N]) -> Point<N> {
+        Point { coordinates }
+    }
     /// Returns true if we're next to this other point
-    pub fn is_neighbour(&self, other: &Point) -> bool {
-        // If the euclidean distance is 1, this is a neighbour
-        // Euclidean distance is the square root of the sum of the squares of
-        // the distances on each dimension
+    pub fn is_neighbour(&self, other: &Point<N>) -> bool {
         assert_eq!(self.coordinates.len(), other.coordinates.len());
-        let distance = self
+        // Get the distance to travel on each dimesnion
+        let mut one_count = 0;
+        for distance in self
             .coordinates
             .iter()
             .zip(other.coordinates.iter())
+            // Count
             // Get the distance squared for each coordinate (x,y,z, etc.)
-            .map(|(a, b)| (*a - b).pow(2) as f32)
-            // Sum them
-            .sum::<f32>()
-            // Take the square root of that
-            .sqrt();
-        distance == 1.0
+            .map(|(a, b)| (a - b).abs())
+        {
+            if distance == 1 {
+                // We need at least one coordinate to be
+                one_count += 1;
+            } else if distance > 1 {
+                // If any dimensions are > 1 away, not neighbours
+                return false;
+            };
+        }
+        // We need at least one dimension to travel along, otherwise it's duplicate point
+        one_count > 0
     }
 }
 
@@ -31,7 +41,7 @@ mod test {
 
     macro_rules! point {
         ($($x:expr),+) => {
-            Point{coordinates: vec! {$($x),+}}
+            Point::new([$($x),+])
         };
     }
 
@@ -42,9 +52,58 @@ mod test {
         let b = point!(5);
         assert!(a.is_neighbour(&b));
         assert!(b.is_neighbour(&a));
-        // 1 dim same point = not neighbour
+        // Same point = not neighbour
         let b = point!(4);
         assert!(!a.is_neighbour(&b));
         assert!(!b.is_neighbour(&a));
+    }
+
+    #[test]
+    fn test_is_neighbour_2_dim() {
+        // 2 dimensional
+        let a = point!(4, 4);
+        let b = point!(4, 5);
+        assert!(a.is_neighbour(&b));
+        assert!(b.is_neighbour(&a));
+        assert!(a.is_neighbour(&point!(3, 3)));
+        assert!(a.is_neighbour(&point!(3, 4)));
+        assert!(a.is_neighbour(&point!(4, 3)));
+        assert!(a.is_neighbour(&point!(5, 5)));
+        assert!(a.is_neighbour(&point!(5, 3)));
+        assert!(a.is_neighbour(&point!(4, 5)));
+        // Same point = not neighbour
+        assert!(!a.is_neighbour(&point!(4, 4)));
+        // negatives
+        assert!(!a.is_neighbour(&point!(-3, 4)));
+        assert!(!a.is_neighbour(&point!(4, -3)));
+        // A bit too far
+        assert!(!a.is_neighbour(&point!(6, 5)));
+        assert!(!a.is_neighbour(&point!(5, 2)));
+        assert!(!a.is_neighbour(&point!(4, 6)));
+    }
+
+    #[test]
+    fn test_is_neighbour_3_dim() {
+        // 2 dimensional
+        let a = point!(4, 4, 4);
+        let b = point!(4, 5, 3);
+        assert!(a.is_neighbour(&b));
+        assert!(b.is_neighbour(&a));
+        assert!(a.is_neighbour(&point!(3, 3, 3)));
+        assert!(a.is_neighbour(&point!(3, 4, 3)));
+        assert!(a.is_neighbour(&point!(4, 3, 4)));
+        assert!(a.is_neighbour(&point!(5, 5, 3)));
+        assert!(a.is_neighbour(&point!(5, 3, 4)));
+        assert!(a.is_neighbour(&point!(4, 5, 4)));
+        // Same point = not neighbour
+        assert!(!a.is_neighbour(&point!(4, 4, 4)));
+        // negatives
+        assert!(!a.is_neighbour(&point!(-3, 4, 4)));
+        assert!(!a.is_neighbour(&point!(4, -3, 4)));
+        // A bit too far
+        assert!(!a.is_neighbour(&point!(6, 5, 4)));
+        assert!(!a.is_neighbour(&point!(5, 2, 4)));
+        assert!(!a.is_neighbour(&point!(4, 4, 2)));
+        assert!(!a.is_neighbour(&point!(4, 4, 6)));
     }
 }
